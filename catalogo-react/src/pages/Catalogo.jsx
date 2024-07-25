715437cb25f44adf9d367a1a3178bc102acd66b5
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-07-25 08:04:37 +0200

    morning commit

--- catalogo-react/src/pages/Catalogo.jsx
+++ catalogo-react/src/pages/Catalogo.jsx
@@ -3,8 +3,11 @@ import { useState, useEffect } from 'react'
 
 const Catalogo = () => {
 
+    const [errorData, setErrorData] = useState("")
     const [albumsPerPage, setAlbumsPerPage] = useState(8)
+    const [albums, setAlbums] = useState([])
     const [filter, setFilter] = useState("")
+    const [buttonFilterGenre, setButtonFilterGenre] = useState("All")
     const [info, setInfo] = useState({
         count: 0,
         page: 0
@@ -14,19 +17,118 @@ const Catalogo = () => {
         getAlbums("./")
     }, [])
 
-    getAlbums = async (url) => {
-        const respuesta = await fetch url(url)
-        const objeto = respuesta.json
-    } 
+    const getAlbums = async (url) => {
+        const respuesta = await fetch(url)
+        const objeto = await respuesta.json()
+
+        if (objeto.error) {
+            setErrorData("No hay resultados")
+            setAlbums([])
+            setInfo({})
+            return;
+        } else {
+            setErrorData("")
+            setAlbums(objeto.results)
+            setInfo(objeto.info)
+        }
+
+    }
+
+    const handleFilterByName = (e) => {
+        const string = e.target.value;
+        setFilter(string);
+        console.log(string)
+        if (string.trim().length > 3) {
+            getAlbums(`/catalogo-react/public/lib.json/?name=${string.trim()}`)
+        }
+    }
+
+    const handleFilterByGenre = (e) => {
+        const string = e.target.value;
+        setFilter(string)
+        console.log(string)
+        if (string.trim().length > 3) {
+            getAlbums(`/catalogo-react/public/lib.json/?genre=${string.trim()}`)
+        }
+    }
+
+    const handleCombinedFilter = (e) => {
+        const string = e.target.value;
+        setValue(string);
+
+
+        handleFilterByGenre(string);
+        handleFilterByName(string);
+    };
+
+    const handleFilterByGenreButton = (genre) => {
+        setButtonFilterGenre(genre)
+        if (genre === "All") {
+            getAlbums("/catalogo-react/public/lib.json")
+        } else {
+            getAlbums(`/catalogo-react/public/lib.json/?genre=${genre}`)
+        }
+    }
+
+    const filteredAlbums = buttonFilter === "All"
+        ? albums
+        : albums.filter(album => album.genre.toLowerCase() === buttonFilter.toLowerCase())
 
 
     return (
         <main>
-            <section>
+            <section className='catalogContainer'>
+                <div className="filterBlock">
+                    <div className="buttonBlock">
+                        <button className={`${buttonFilterGenre == "Pop" ? "btnA" : ""}`} onClick={() => handleFilterByGenre("Pop")}>Pop</button>
+                        <button className={`${buttonFilterGenre == "Rock" ? "btnA" : ""}`} onClick={() => handleFilterByGenre("Pop")}>Pop</button>
+                        <button className={`${buttonFilterGenre == "Latin" ? "btnA" : ""}`} onClick={() => handleFilterByGenre("Pop")}>Pop</button>
+                        <button className={`${buttonFilterGenre == "Hip-Hop" ? "btnA" : ""}`} onClick={() => handleFilterByGenre("Pop")}>Pop</button>
+                    </div>
+                    <div className="searchbarBlock">
+                        <input type="text"
+                            onChange={handleCombinedFilter}
+                            placeholder='¿Que te gustaría escuchar? (4 chars)'
+                            style={{ width: "400px" }}
+                            value={filter}
+                        />
+                        {filter}
 
+                        <button className='x' style={{}} onClick={
+                            () => {
+                                setFilter("")
+                                getAlbums("/catalogo-react/public/lib.json")
+                            }
+                        }>x</button>
+                    </div>
+                </div>
+
+                <div className="albumCatalog">
+                    {errorData && <div>{errorData}</div>}
+                    {
+                        albums.map((albums, index) => <AlbumCard key={album.id} {...album} index={index} />
+                        );
+                        }
+                </div>
             </section>
         </main>
     );
 }
+
+const AlbumCard = ({ image, name, name_artist, album_date, times_played, likes, genre}) => {
+    return (
+        <article className="Card">
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <p>{nameArtist}</p>
+            <p>{albumDate}</p>
+            <p>{timesPlayed}</p>
+            <p>{likes}</p>
+            <p>{genre}</p>
+        </article>
+    )
+}
+
+
 
 export default Catalogo;
